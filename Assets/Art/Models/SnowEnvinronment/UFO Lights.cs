008e2163d7223e9d281e4fe41a5739d18a4b415f
AC-GOps <agustin.cles@gmail.com>
2024-11-07 18:11:31 +0100

    Add Ice Cavern Level + new assets and mats
    Ice Cavern Level + new assets and mats

---
+++ Assets/Art/Models/SnowEnvinronment/UFO Lights.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using UnityEngine;
+
+public class MaterialFlicker : MonoBehaviour
+{
+    public Material lightsOFF;
+    public Material lightsON;
+    public float minTime;
+    public float maxTime;
+
+    private MeshRenderer meshRenderer;
+    private int targetSubMeshIndex = 1;
+
+    void Start()
+    {
+        meshRenderer = GetComponent<MeshRenderer>();
+        StartCoroutine(FlickerMaterial(targetSubMeshIndex));
+    }
+
+    private IEnumerator FlickerMaterial(int index)
+    {
+        while (true)
+        {
+            Material[] materials = meshRenderer.sharedMaterials;
+
+            if (ReferenceEquals(materials[index], lightsOFF))
+            {
+                materials[index] = lightsON;
+            }
+            else if (ReferenceEquals(materials[index], lightsON))
+            {
+                materials[index] = lightsOFF;
+            }
+            else
+            {
+                materials[index] = lightsOFF;
+            }
+
+            meshRenderer.sharedMaterials = materials;
+
+            yield return new WaitForSeconds(Random.Range(minTime, maxTime));
+        }
+    }
+}
