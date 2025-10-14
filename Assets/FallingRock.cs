542bb407910486d748daf5faa35558246ccf71e4
21stFret <79184648+21stFret@users.noreply.github.com>
2025-10-14 12:31:06 +0100

    new breakable pillar

--- Assets/FallingRock.cs
+++ Assets/FallingRock.cs
@@ -25,6 +25,10 @@ public class FallingRock : MonoBehaviour
             {
                 targetHealth.TakeDamage(damageAmount);
             }
+            if(hitCollider.CompareTag("BigEnvironment"))
+            {
+                hitCollider.GetComponent<BreakableEnvironment>().Hit();
+            }
         }
 
     }
